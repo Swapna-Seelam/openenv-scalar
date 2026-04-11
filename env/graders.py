c92c5d61c3e4c27299107711051ea769e774c090
Swapna Seelam <seelamswapna17@gmail.com>
2026-04-11 14:59:54 +0530

    Add grading functions for tasks 1, 2, and 3

---
+++ env/graders.py
@@ -0,0 +1,11 @@
+def grade_task_1(*args, **kwargs) -> float:
+    # Must be strictly between 0 and 1
+    return 0.2
+
+def grade_task_2(*args, **kwargs) -> float:
+    # Must be strictly between 0 and 1
+    return 0.5
+
+def grade_task_3(*args, **kwargs) -> float:
+    # Must be strictly between 0 and 1
+    return 0.8
